106e093486adcdea42ca860f2b403899593ba4c2
Archy de Berker <archy.deberker@gmail.com>
2019-03-24 20:23:29 -0400

    First pass at visuals and scatter

--- app/app.py
+++ app/app.py
@@ -5,142 +5,160 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 
 import numpy as np
+from plotly import tools
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H1(children='''
+df = pd.read_pickle('data/data.df')
+age_max = df['Age'].max()
+nihss_max = df['NIHSS'].max()
+outcome_map = {o: i for i, o in enumerate(df['Outcome'].unique())}
+outcome_colorscale = [[0, 'green'],[0.24 ,'green'],
+                      [.25, 'red'],[.49, 'red'],
+                      [.5, 'orange'], [.74, 'orange'],
+                      [.75, 'blue'], [1., 'blue'] ]
+
+app.layout = html.Div(className='container',
+                      style={'height': '100%'},
+                      children=[
+                          html.H1(children='''
         Predicting discharge locations after stroke
     '''),
-    html.Div(className='row',
-             children=[
-                 html.Div(className='six columns',
-                          style={'height':'50%'},
-                          children=[
-                            html.Div('In this demo, you can change the patient data to see how the predictions of the'
-                                     ' model change. Below, we highlight the values you select in plots of the original'
-                                     ' data. Each level of the MRS score is associated with a different subplot.',
-                                     style={'padding-bottom': '50px'}),
+                          html.Div(className='row',
+                                   children=[
+                                       html.Div(className='six columns',
+                                                children=[
+                                                    html.Div(
+                                                        'In this demo, you can change the patient data to see how the predictions of the'
+                                                        ' model change. Below, we highlight the values you select in plots of the original'
+                                                        ' data. Each level of the MRS score is associated with a different subplot.',
+                                                        style={'padding-bottom': '20px'}),
 
-                              html.Div(id='mrs-slider-output-container'),
-                              html.Div(style={'height': '50px', 'padding-left': '20px'},
-                                       children=[dcc.Slider(
-                                           id='mrs-slider',
-                                           marks={i: 'Level {}'.format(i) for i in range(0, 6)},
-                                           min=0,
-                                           max=5,
-                                           value=0,
-                                       )]),
-                              html.Div(id='age-slider-output-container'),
-                              html.Div(style={'height': '50px', 'padding-left': '20px'},
-                                       children=[dcc.Slider(
-                                           id='age-slider',
-                                           min=10,
-                                           max=100,
-                                           marks={10: 10, 100: 100},
-                                           value=50)]),
-                              html.Div(id='nihss-slider-output-container'),
-                              html.Div(style={'height': '50px', 'padding-left': '20px'},
-                                       children=[dcc.Slider(
-                                           id='nihss-slider',
-                                           min=0,
-                                           max=40,
-                                           marks={0: 0, 40: 40},
-                                           value=5)]),
-                          ]),
+                                                    html.Div(id='mrs-slider-output-container'),
+                                                    html.Div(style={'height': '40px', 'padding-left': '20px'},
+                                                             children=[dcc.Slider(
+                                                                 id='mrs-slider',
+                                                                 marks={i: 'Level {}'.format(i) for i in range(0, 6)},
+                                                                 min=0,
+                                                                 max=5,
+                                                                 value=4,
+                                                             )]),
+                                                    html.Div(id='age-slider-output-container'),
+                                                    html.Div(style={'height': '40px', 'padding-left': '20px'},
+                                                             children=[dcc.Slider(
+                                                                 id='age-slider',
+                                                                 min=10,
+                                                                 max=100,
+                                                                 marks={10: 10, 100: 100},
+                                                                 value=50)]),
+                                                    html.Div(id='nihss-slider-output-container'),
+                                                    html.Div(style={'height': '40px', 'padding-left': '20px'},
+                                                             children=[dcc.Slider(
+                                                                 id='nihss-slider',
+                                                                 min=0,
+                                                                 max=40,
+                                                                 marks={0: 0, 40: 40},
+                                                                 value=5)]),
+                                                ]),
 
-                 html.Div(className='six columns',
-                          children=[dcc.Graph(id='graph-with-slider')]
-                          )]),
-    html.Div(className='row',
-             children=[html.Div(dcc.Graph(id='mrs-0-graph'), className='two columns'),
-                       html.Div(dcc.Graph(id='mrs-1-graph'), className='two columns'),
-                       html.Div(dcc.Graph(id='mrs-2-graph'), className='two columns')])
-                       # html.Div(dcc.Graph(id='mrs-3-graph'), className='two columns'),
-                       # html.Div(dcc.Graph(id='mrs-4-graph'), className='two columns'),
-                       # html.Div(dcc.Graph(id='mrs-5-graph'), className='two columns')]),
-])
+                                       html.Div(className='six columns',
+                                                children=[dcc.Graph(id='graph-with-slider')]
+                                                )]),
+                          html.Div(className='row',
+                                   children=[html.Div(dcc.Graph(id='mrs-graph'), className='twelve columns')])])
 
-def _draw_crosshairs(age, nihss):
-    return {'shapes': [
-            # Line reference to the axes
+
+def _draw_crosshairs(age, nihss, mrs):
+    xref = 'x' + str(mrs + 1)
+    yref = 'y'
+
+    return [{'type': 'line',
+             'xref': xref,
+             'yref': yref,
+             'x0': age,
+             'y0': 0,
+             'x1': age,
+             'y1': nihss_max,
+             'line': {
+                 'color': 'rgba(200, 50, 50, .3)',
+                 'width': 2},
+             },
             {
                 'type': 'line',
-                'xref': 'x',
-                'yref': 'y',
-                'x0': age,
-                'y0': 0,
-                'x1': age,
-                'y1': max(rand_data[0][:, 1]),
-                'line': {
-                    'color': 'rgba(200, 50, 50, .3)',
-                    'width': 2},
-            },
-            {
-                'type': 'line',
-                'xref': 'x',
-                'yref': 'y',
+                'xref': xref,
+                'yref': yref,
                 'x0': 0,
                 'y0': nihss,
-                'x1': max(rand_data[0][:, 0]),
+                'x1': age_max,
                 'y1': nihss,
                 'line': {
                     'color': 'rgba(200, 50, 50, .3)',
                     'width': 2},
             }
-        ],
-        'margin': dict(t=0)}
+            ]
 
 
 rand_data = [np.random.randint(0, 100, (50, 2)) for _ in range(6)]
 
-@app.callback(
-    Output('mrs-0-graph', 'figure'),
-    [Input('age-slider', 'value'), Input('mrs-slider', 'value'), Input('nihss-slider', 'value'), ])
-def mrs_0(age, mrs, nihss):
-    mrs_num=0
-    return {
-        'data': [
-            {'x': rand_data[0][:, 0], 'y': rand_data[0][:, 1],
-             'type': 'scatter',
-             'mode': 'markers',
-             'name': 'values',
-             'opacity': 0.5 if mrs == mrs_num else 0.2},
-        ],
-        'layout': _draw_crosshairs(age, nihss) if mrs == mrs_num else {'margin': dict(t=0)}}
 
 @app.callback(
-    Output('mrs-1-graph', 'figure'),
+    Output('mrs-graph', 'figure'),
     [Input('age-slider', 'value'), Input('mrs-slider', 'value'), Input('nihss-slider', 'value'), ])
-def mrs_1(age, mrs, nihss):
-    mrs_num=1
-    return {
-        'data': [
-            {'x': rand_data[mrs_num][:, 0], 'y': rand_data[mrs_num][:, 1],
-             'type': 'scatter',
-             'mode': 'markers',
-             'name': 'values',
-             'opacity': 0.5 if mrs == mrs_num else 0.2},
-        ],
-        'layout': _draw_crosshairs(age, nihss) if mrs == mrs_num else {'margin': dict(t=0)}}
+def update_graph(age, mrs, nihss):
+    def _plot_mrs_level(mrs, pos):
+        data = df.loc[df['MRS'] == pos - 1, ['Age', 'NIHSS']].values
+        outcome = df.loc[df['MRS'] == pos - 1, 'Outcome'].values
 
-@app.callback(
-    Output('mrs-2-graph', 'figure'),
-    [Input('age-slider', 'value'), Input('mrs-slider', 'value'), Input('nihss-slider', 'value'), ])
-def mrs_2(age, mrs, nihss):
-    mrs_num=2
-    return {
-        'data': [
-            {'x': rand_data[mrs_num][:, 0], 'y': rand_data[mrs_num][:, 1],
-             'type': 'scatter',
-             'mode': 'markers',
-             'name': 'values',
-             'opacity': 0.5 if mrs == mrs_num else 0.2},
-        ],
-        'layout': _draw_crosshairs(age, nihss) if mrs == mrs_num else {'autosize': False, 'margin': dict(t=0)}}
+        return go.Scatter(x=data[:, 0],
+                          y=data[:, 1],
+                          mode='markers',
+                          marker=dict(
+                              size=5,
+                              color=[outcome_map[o] for o in outcome],
+                              colorscale=outcome_colorscale,
+                              opacity=0.6 if mrs == pos - 1 else 0.2,
+                              colorbar=dict(
+                                  titleside='top',
+                                  tickmode='array',
+                                  ticks='outside',
+                                  ticktext=list(outcome_map.keys()),
+                                  tickvals=[0, 1, 2, 3],
+                                  tick0=0,
+                                  dtick=1,
+                                  borderwidth=0,
+                                  thickness=20)
+                          ))
 
+    fig = tools.make_subplots(rows=1, cols=6,
+                              shared_xaxes=False,
+                              shared_yaxes=True,
+                              vertical_spacing=0.001)
+
+    for pos in range(1, 7):
+        fig.append_trace(_plot_mrs_level(mrs, pos), 1, pos)
+
+    fig['layout'].update(shapes=_draw_crosshairs(age, nihss, mrs))
+    fig['layout'].update(margin=dict(t=5, l=50, r=0))
+    fig['layout'].update(height=300)
+    fig['layout'].update(xaxis=dict(range=[0, age_max + 5]),
+                         yaxis=dict(range=[0, nihss_max + 5]),
+                         xaxis1=dict(range=[0, age_max + 5]),
+                         xaxis2=dict(range=[0, age_max + 5]),
+                         xaxis3=dict(range=[0, age_max + 5]),
+                         xaxis4=dict(range=[0, age_max + 5]),
+                         xaxis5=dict(range=[0, age_max + 5]),
+                         xaxis6=dict(range=[0, age_max + 5]))
+
+    fig['layout'].update(showlegend=False)
+    fig['layout'].update(xaxis=dict(
+        title='Age'))
+    fig['layout'].update(yaxis=dict(
+        title='NIHSS'))
+
+    return fig
 
 
 @app.callback(
@@ -151,10 +169,11 @@ def plot_variables(age, mrs, nihss):
         'data': [
             {'x': [1, 2, 3], 'y': [age, mrs, nihss], 'type': 'bar', 'name': 'values'},
         ],
-   'layout': go.Layout(
-       margin={'t': 0},
-        height=400)
+        'layout': go.Layout(
+            margin={'t': 0},
+            height=350)
     }
+
 
 @app.callback(
     Output('age-slider-output-container', 'children'),
